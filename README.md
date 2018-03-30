4aca3edd4a71af1c369dd429410dfb9a92798fd9
sainathGit <sainathc6@gmail.com>
2018-03-30 16:25:54 +0530

    created wireframes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # saco
 its a basic quizzing app, which asks series of questions and gives the result report at the end.
+its made using tha django framework.
---
+++ saco_django/takequiz/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Question(models.Model):
+	q_no			 = models.IntegerField(default=0)
+	q_text		 = models.charField(max
+	q_image		 = 
+	q_ans  		 = models.IntegerField(default=0)
+	q_solution = 
+
+
+
+	
